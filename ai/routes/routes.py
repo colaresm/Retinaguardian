e4530cfeb0ac28d489640574ac7a4d140d810325
Marcelo Colares <marcelocolares@MacBook-Air-de-Marcelo.local>
2025-02-20 20:22:03 -0300

    Insert predict endpoint

--- ai/routes/routes.py
+++ ai/routes/routes.py
@@ -1,22 +1,33 @@
 from flask import Blueprint, request, jsonify
 from tensorflow import keras
-
+import cv2
+import numpy as np
+from utils import get_class_name
 
 classify_bp = Blueprint('classify', __name__)
 
 @classify_bp.route('/classify', methods=['POST'])
 def classify_image():
-    model = keras.models.load_model("models/classification_model.keras")
-
-    """Recebe uma imagem e retorna a classificação da RD."""
     if 'file' not in request.files:
         return jsonify({"error": "Nenhuma imagem enviada"}), 400
 
     file = request.files['file']
-   
-    
-    
-    return jsonify({"diagnosis": "teste"})
+    file_bytes = np.frombuffer(file.read(), np.uint8) 
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)  
+
+    if image is None:
+        return jsonify({"error": "Falha ao processar a imagem"}), 400
+
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_LINEAR)
+
+    image = np.expand_dims(image, axis=0)  
+
+    model = keras.models.load_model("models/classification_model.keras")
+    prediction = model.predict(image)[0][0]   
+
+    predicted_class = 1 if prediction >= 0.5 else 0
+    return jsonify({"prediction":get_class_name(int(predicted_class)) })
+
 
 
 #the env name is venv
